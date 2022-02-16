1495e40e816d68fcfb65639a24600e83cc0e4406
F5G9IFY <F5G9IFY@DESKTOP-IHBFITS>
2022-02-16 10:12:28 +0530

    Add project files.

---
+++ MyForm.cpp
@@ -0,0 +1,20 @@
+#include "MyForm.h"
+
+using namespace System;
+
+using namespace System::Windows::Forms;
+
+[STAThread]
+
+
+void main(array<String^>^ args) {
+
+	Application::EnableVisualStyles;
+
+	Application::SetCompatibleTextRenderingDefault;
+
+	demo::MyForm form;
+
+	Application::Run(%form);
+
+}
---
+++ MyForm.h
@@ -0,0 +1,137 @@
+#pragma once
+
+namespace demo {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::ListView^ listView1;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	protected:
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->listView1 = (gcnew System::Windows::Forms::ListView());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->Location = System::Drawing::Point(58, 9);
+			this->label1->MinimumSize = System::Drawing::Size(0, 10);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(148, 38);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"ADD TASK";
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(294, 53);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 1;
+			this->button1->Text = L"ADD";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// listView1
+			// 
+			this->listView1->AllowColumnReorder = true;
+			this->listView1->AllowDrop = true;
+			this->listView1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F, static_cast<System::Drawing::FontStyle>((System::Drawing::FontStyle::Bold | System::Drawing::FontStyle::Italic)),
+				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
+			this->listView1->HideSelection = false;
+			this->listView1->Location = System::Drawing::Point(61, 108);
+			this->listView1->Name = L"listView1";
+			this->listView1->Size = System::Drawing::Size(255, 244);
+			this->listView1->Sorting = System::Windows::Forms::SortOrder::Ascending;
+			this->listView1->TabIndex = 2;
+			this->listView1->UseCompatibleStateImageBehavior = false;
+			// 
+			// textBox1
+			// 
+			this->textBox1->AcceptsReturn = true;
+			this->textBox1->AcceptsTab = true;
+			this->textBox1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->textBox1->Location = System::Drawing::Point(61, 50);
+			this->textBox1->Multiline = true;
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(183, 30);
+			this->textBox1->TabIndex = 3;
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(465, 388);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->listView1);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label1);
+			this->Name = L"MyForm";
+			this->Text = L"ToDoList";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+
+		String ^  s = textBox1->Text;
+
+		//if (s->Equals( "" ) ) return ;
+
+		textBox1->Text = "";
+
+		listView1->Items->Add(s);
+
+	}
+
+};
+}
